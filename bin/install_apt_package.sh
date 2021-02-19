b4e3b3b0e7a3477367e08b82eba1d2586bcb94ec
k5-mot-wsl2 <keigo.mot+wsl2@gmail.com>
2021-02-19 20:01:34 +0900

    anyenvインストールスクリプトを修正、aptインストールスクリプトに追加

--- bin/install_apt_package.sh
+++ bin/install_apt_package.sh
@@ -10,25 +10,30 @@ sudo apt autoclean
 sudo sed -i.bak -r 's!(deb|deb-src) \S+!\1 mirror://mirrors.ubuntu.com/mirrors.txt!' /etc/apt/
 
 ## Install packages
+sudo apt install -y make
+sudo apt install -y gcc
+sudo apt install -y libssl-dev
+sudo apt install -y libreadline-dev
+sudo apt install -y zlib1g-dev
+sudo apt install -y libbz2-dev
+sudo apt install -y libsqlite3-dev
+sudo apt install -y libncurses5-dev
+sudo apt install -y libncursesw5-dev
+sudo apt install -y npm
 sudo apt install -y vim
 sudo apt install -y emacs
 sudo apt install -y neovim
 sudo apt install -y zsh
 sudo apt install -y tmux
-sudo apt install -y make
 sudo apt install -y curl
 sudo apt install -y llvm
 sudo apt install -y sqlite3
-sudo apt install -y libssl-dev
-sudo apt install -y libbz2-dev
-sudo apt install -y libreadline-dev
-sudo apt install -y libsqlite3-dev
-sudo apt install -y libncurses5-dev
-sudo apt install -y libncursesw5-dev
 sudo apt install -y python-tk
 sudo apt install -y python3-tk
 sudo apt install -y tk-dev
 sudo apt install -y aria2
 sudo apt install -y octave
+sudo npm install -g n
+sudo n stable
 #sudo apt install -y ccache
 
