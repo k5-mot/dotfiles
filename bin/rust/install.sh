6ebee246f440b333f4c6bbb48dd141f204f5965f
k5-mot <k5.mot.888@gmail.com>
2021-04-03 00:37:42 +0900

    WIP

--- bin/rust/install.sh
+++ bin/rust/install.sh
@@ -4,6 +4,6 @@
 cd $HOME
 
 rm -rf $HOME/.cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
