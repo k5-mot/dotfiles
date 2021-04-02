670d196a799e7eee54f8b9eb2a1922558f294def
k5-mot <k5.mot.888@gmail.com>
2021-04-03 01:28:25 +0900

    WIP

--- bin/porg/porg.sh
+++ bin/porg/porg.sh
@@ -1,28 +1,42 @@
 #!/usr/bin/env bash
 
+##
 ## porg
+##
+
+# Create source directory.
 cd $HOME
-mkdir -p $HOME/.local
-mkdir -p $HOME/.local/src
+mkdir -pv $HOME/.local
+mkdir -pv $HOME/.local/src
 cd $HOME/.local/src
+
+# Remove source directory.
 rm -rf $HOME/.local/src/porg
-mkdir -p $HOME/.local/src/porg
+
+# Download source.
+mkdir -pv $HOME/.local/src/porg
 cd $HOME/.local/src/porg/
 wget --content-disposition https://sourceforge.net/projects/porg/files/latest/download
 tar -xzvf porg-*.tar.gz
 cd porg-*/
-mkdir -p $HOME/.local/usr
-mkdir -p $HOME/.local/usr/bin
-mkdir -p $HOME/.local/usr/lib
-mkdir -p $HOME/.local/etc
-mkdir -p $HOME/.local/var
-mkdir -p $HOME/.local/var/log
-mkdir -p $HOME/.local/var/log/porg
+
+# Create local bin, lib and more...
+mkdir -pv $HOME/.local/usr
+mkdir -pv $HOME/.local/usr/bin
+mkdir -pv $HOME/.local/usr/lib
+mkdir -pv $HOME/.local/etc
+mkdir -pv $HOME/.local/var
+mkdir -pv $HOME/.local/var/log
+mkdir -pv $HOME/.local/var/log/porg
+
+# Install
 ./configure --prefix=$HOME/.local/usr \
   --sysconfdir=$HOME/.local/etc \
   --with-porg-logdir=$HOME/.local/var/log/porg \
   --disable-grop
 make
 ./porg/porg -lD make install
-porg porg
+
+./porg/porg porg
 cd $HOME
+
