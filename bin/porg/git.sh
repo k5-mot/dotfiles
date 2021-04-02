593b2bdab65c9deb2e9ebb11e9971997e974f388
k5-mot <k5.mot.888@gmail.com>
2021-04-03 01:35:15 +0900

    WIP

--- bin/porg/git.sh
+++ bin/porg/git.sh
@@ -2,16 +2,25 @@
 
 ## Git
 
+# Change dir.
 cd $HOME
 cd $HOME/.local/src
+
+# Remove package.
 if (type "porg" > /dev/null 2>&1); then
   porg -r git
 fi
 rm -rf $HOME/.local/src/git
+
+# Download package.
 git clone https://github.com/git/git.git
 cd $HOME/.local/src/git
+
+# Install package.
 make prefix=$HOME/.local/usr
 export GIT_VERSION=$(./git --version | head -1 | awk '{print $3}')
 porg -lp git-$GIT_VERSION make prefix=$HOME/.local/usr install
+
+# Check package.
 porg git
 cd $HOME
