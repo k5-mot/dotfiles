65f8b62eb1932b9e769222770ca2a56519b42eec
k5-mot-cos <k5.mot.888@gmail.com>
2021-06-17 17:40:10 +0900

    WIP

--- scripts/porg/neovim.sh
+++ scripts/porg/neovim.sh
@@ -2,17 +2,27 @@
 
 ## neovim
 
+# Change dir
 cd $HOME
 cd $HOME/.local/src
+
+# Remove package
 if (type "porg" > /dev/null 2>&1); then
   porg -r neovim
 fi
-rm -rf $HOME/.local/src/neovim 
+rm -rf $HOME/.local/src/neovim
+
+# Download package
 git clone https://github.com/neovim/neovim.git
 cd $HOME/.local/src/neovim
-rm -rf build/
+
+# Install package
 git checkout stable
 make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local/usr"
 export NEOVIM_VERSION=$(./build/bin/nvim --version | head -1 | awk '{print $2}' | sed "s/v//g")
-porg -lp neovim-$NEOVIM_VERSION make CMAKE_INSTALL_PREFIX=$HOME/.local/usr install
+porg -lp neovim-$NEOVIM_VERSION "make CMAKE_INSTALL_PREFIX=$HOME/.local/usr install"
+
+# Check package
+porg neovim
 cd $HOME
+
