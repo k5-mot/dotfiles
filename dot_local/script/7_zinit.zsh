212efd0243b07500c603cf7e54723c061fa2a909
k5-mot <34744243+k5-mot@users.noreply.github.com>
2024-03-21 00:52:29 +0900

    :sparkles: Add configs.

--- dot_local/script/7_zinit.zsh
+++ dot_local/script/7_zinit.zsh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 
-source ~/.zshrc
+source "${HOME}/.zshenv"
+source "${XDG_CONFIG_HOME}/zsh/zshenv"
+source "${XDG_CONFIG_HOME}/zsh/zshrc"
+
 zinit self-update
 zinit update --all
 zinit delete --clean
