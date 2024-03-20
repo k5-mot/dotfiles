55f639e6ed441ea90cbce1ee520578957d9aa37b
k5-mot <34744243+k5-mot@users.noreply.github.com>
2024-03-21 01:07:26 +0900

    :sparkles: Add configs.

--- dot_local/script/3-kubectl.sh
+++ dot_local/script/3-kubectl.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 ### docker-compose
-if [ ! -x "docker-compose" ]; then
+if ! command -v "docker-compose" &> /dev/null; then
     sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     docker-compose -v
 fi
 
 ### minikube
-if [ ! -x "minikube" ]; then
+if ! command -v "minikube" &> /dev/null; then
     curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
     chmod +x minikube-linux-amd64
     sudo install minikube-linux-amd64 /usr/local/bin/minikube
@@ -17,7 +17,7 @@ if [ ! -x "minikube" ]; then
 fi
 
 ### Kubectl
-if [ ! -x "kubectl" ]; then
+if ! command -v "kubectl" &> /dev/null; then
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     chmod +x ./kubectl
     sudo mv -f ./kubectl /usr/local/bin
@@ -25,7 +25,7 @@ if [ ! -x "kubectl" ]; then
 fi
 
 ### eksctl
-if [ ! -x "eksctl" ]; then
+if ! command -v "eksctl" &> /dev/null; then
     ARCH=amd64
     PLATFORM=$(uname -s)_$ARCH
     curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
