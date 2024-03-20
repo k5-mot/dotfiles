bb4142088a09b7b7d20a5746319797f9a3e7bced
k5-mot <34744243+k5-mot@users.noreply.github.com>
2024-03-20 20:55:51 +0900

    :sparkles: Add configs.

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -198,9 +198,10 @@ packer.startup(function(use)
     -- }
 
     -- Startup
-    use {
-        "nvimdev/dashboard-nvim",
-    }
+    -- use {
+    --     "nvimdev/dashboard-nvim",
+    --     requires = {'nvim-tree/nvim-web-devicons'}
+    -- }
 
     -- Colorscheme
     use 'dracula/vim'
@@ -228,66 +229,66 @@ vim.opt.termguicolors = true
 vim.cmd('colorscheme darkblue')
 -- require("bufferline").setup({})
 
-local db = require('dashboard')
-db.custom_header = {
-"",
-"",
-"",
-"",
-"",
-"██╗░░██╗███████╗░░░░░░███╗░░░███╗░█████╗░████████╗██╗░██████╗     ███╗░░██╗███████╗░█████╗░██╗░░░██╗██╗███╗░░░███╗",
-"██║░██╔╝██╔════╝░░░░░░████╗░████║██╔══██╗╚══██╔══╝╚█║██╔════╝     ████╗░██║██╔════╝██╔══██╗██║░░░██║██║████╗░████║",
-"█████═╝░██████╗░█████╗██╔████╔██║██║░░██║░░░██║░░░░╚╝╚█████╗░     ██╔██╗██║█████╗░░██║░░██║╚██╗░██╔╝██║██╔████╔██║",
-"██╔═██╗░╚════██╗╚════╝██║╚██╔╝██║██║░░██║░░░██║░░░░░░░╚═══██╗     ██║╚████║██╔══╝░░██║░░██║░╚████╔╝░██║██║╚██╔╝██║",
-"██║░╚██╗██████╔╝░░░░░░██║░╚═╝░██║╚█████╔╝░░░██║░░░░░░██████╔╝     ██║░╚███║███████╗╚█████╔╝░░╚██╔╝░░██║██║░╚═╝░██║",
-"╚═╝░░╚═╝╚═════╝░░░░░░░╚═╝░░░░░╚═╝░╚════╝░░░░╚═╝░░░░░░╚═════╝░     ╚═╝░░╚══╝╚══════╝░╚════╝░░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝",
-"",
-"",
-"",
-"█▀▀ █░█ ▄▀█ █▄░█ █▀▀ █▀▀   █▄▄ █▀▀ █▀▀ █▀█ █▀█ █▀▀   █▄█ █▀█ █░█   █░█ ▄▀█ █░█ █▀▀   ▀█▀ █▀█ ░",
-"█▄▄ █▀█ █▀█ █░▀█ █▄█ ██▄   █▄█ ██▄ █▀░ █▄█ █▀▄ ██▄   ░█░ █▄█ █▄█   █▀█ █▀█ ▀▄▀ ██▄   ░█░ █▄█ ▄",
-"",
-"",
-}
+-- local db = require('dashboard')
+-- db.custom_header = {
+-- "",
+-- "",
+-- "",
+-- "",
+-- "",
+-- "██╗░░██╗███████╗░░░░░░███╗░░░███╗░█████╗░████████╗██╗░██████╗     ███╗░░██╗███████╗░█████╗░██╗░░░██╗██╗███╗░░░███╗",
+-- "██║░██╔╝██╔════╝░░░░░░████╗░████║██╔══██╗╚══██╔══╝╚█║██╔════╝     ████╗░██║██╔════╝██╔══██╗██║░░░██║██║████╗░████║",
+-- "█████═╝░██████╗░█████╗██╔████╔██║██║░░██║░░░██║░░░░╚╝╚█████╗░     ██╔██╗██║█████╗░░██║░░██║╚██╗░██╔╝██║██╔████╔██║",
+-- "██╔═██╗░╚════██╗╚════╝██║╚██╔╝██║██║░░██║░░░██║░░░░░░░╚═══██╗     ██║╚████║██╔══╝░░██║░░██║░╚████╔╝░██║██║╚██╔╝██║",
+-- "██║░╚██╗██████╔╝░░░░░░██║░╚═╝░██║╚█████╔╝░░░██║░░░░░░██████╔╝     ██║░╚███║███████╗╚█████╔╝░░╚██╔╝░░██║██║░╚═╝░██║",
+-- "╚═╝░░╚═╝╚═════╝░░░░░░░╚═╝░░░░░╚═╝░╚════╝░░░░╚═╝░░░░░░╚═════╝░     ╚═╝░░╚══╝╚══════╝░╚════╝░░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝",
+-- "",
+-- "",
+-- "",
+-- "█▀▀ █░█ ▄▀█ █▄░█ █▀▀ █▀▀   █▄▄ █▀▀ █▀▀ █▀█ █▀█ █▀▀   █▄█ █▀█ █░█   █░█ ▄▀█ █░█ █▀▀   ▀█▀ █▀█ ░",
+-- "█▄▄ █▀█ █▀█ █░▀█ █▄█ ██▄   █▄█ ██▄ █▀░ █▄█ █▀▄ ██▄   ░█░ █▄█ █▄█   █▀█ █▀█ ▀▄▀ ██▄   ░█░ █▄█ ▄",
+-- "",
+-- "",
+-- }
 
-db.custom_center = {
-    {
-        icon = '  ',
-        desc = 'Recently latest session                 ',
-        shortcut = 'SPC s l',
-        action ='SessionLoad'
-    },
-    {
-        icon = '  ',
-        desc = 'Recently opened files                   ',
-        action =  'DashboardFindHistory',
-        shortcut = 'SPC f h'
-    },
-    {
-        icon = '  ',
-        desc = 'Find  File                              ',
-        action = 'Telescope find_files find_command=rg,--hidden,--files',
-        shortcut = 'SPC f f'
-    },
-    {
-        icon = '  ',
-        desc ='File Browser                            ',
-        action =  'Telescope file_browser',
-        shortcut = 'SPC f b'
-    },
-    {
-        icon = '  ',
-        desc = 'Find  word                              ',
-        action = 'Telescope live_grep',
-        shortcut = 'SPC f w'
-    },
-    -- {
-    --     icon = '  ',
-    --     desc = 'Open Personal dotfiles                  ',
-    --     action = 'Telescope dotfiles path=' .. os.getenv('HOME') ..'Documents/GitHub/dotfiles',
-    --     shortcut = 'SPC f d'
-    -- },
-}
+-- db.custom_center = {
+--     {
+--         icon = '  ',
+--         desc = 'Recently latest session                 ',
+--         shortcut = 'SPC s l',
+--         action ='SessionLoad'
+--     },
+--     {
+--         icon = '  ',
+--         desc = 'Recently opened files                   ',
+--         action =  'DashboardFindHistory',
+--         shortcut = 'SPC f h'
+--     },
+--     {
+--         icon = '  ',
+--         desc = 'Find  File                              ',
+--         action = 'Telescope find_files find_command=rg,--hidden,--files',
+--         shortcut = 'SPC f f'
+--     },
+--     {
+--         icon = '  ',
+--         desc ='File Browser                            ',
+--         action =  'Telescope file_browser',
+--         shortcut = 'SPC f b'
+--     },
+--     {
+--         icon = '  ',
+--         desc = 'Find  word                              ',
+--         action = 'Telescope live_grep',
+--         shortcut = 'SPC f w'
+--     },
+--     -- {
+--     --     icon = '  ',
+--     --     desc = 'Open Personal dotfiles                  ',
+--     --     action = 'Telescope dotfiles path=' .. os.getenv('HOME') ..'Documents/GitHub/dotfiles',
+--     --     shortcut = 'SPC f d'
+--     -- },
+-- }
 
 --   local function footer()
 --     -- Number of plugins
