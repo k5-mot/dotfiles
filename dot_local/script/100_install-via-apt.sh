380aea676b63f08d576f02f8c8adb79ad841bb3e
k5-mot <34744243+k5-mot@users.noreply.github.com>
2025-07-02 23:54:42 +0900

    A

--- dot_local/script/100_install-via-apt.sh
+++ dot_local/script/100_install-via-apt.sh
@@ -11,48 +11,44 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
 sudo apt install -y ca-certificates curl gnupg
-sudo apt install -y --no-install-recommends unzip vim tmux zsh
+sudo apt install -y --no-install-recommends unzip git wget
+sudo apt install -y --no-install-recommends vim tmux zsh
 sudo chsh -s /bin/zsh
 
 ### Requirements
 
-#  git cmake pkg-config curl wget xclip zsh cmake unzip curl
+# Python
+# https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+sudo apt install -y --no-install-recommends \
+    build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
-# # Python
-# # https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-# sudo apt install -y \
-#     build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+# Ruby
+# https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
+sudo apt install -y --no-install-recommends \
+    autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
 
-# # Ruby
-# # https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
-# sudo apt install -y \
-#     autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
+# Node.js
+# https://github.com/nodejs/node/blob/main/BUILDING.md#building-nodejs-on-supported-platforms
+sudo apt install -y --no-install-recommends \
+    python3 g++ make python3-pip
 
-# # Node.js
-# # https://github.com/nodejs/node/blob/main/BUILDING.md#building-nodejs-on-supported-platforms
-# sudo apt install -y \
-#     python3 g++ make python3-pip
+# Lua
+# https://www.lua.org/manual/5.4/readme.html
+sudo apt install -y --no-install-recommends \
+    make
 
-# # Lua
-# # https://www.lua.org/manual/5.4/readme.html
-# sudo apt install -y \
-#     make
+# Vim
+# https://github.com/vim/vim/blob/master/src/INSTALL
+sudo apt install -y --no-install-recommends \
+    git make clang libtool-bin libxt-dev libgtk-3-dev libpython3-dev valgrind xclip
 
-# # Vim
-# # https://github.com/vim/vim/blob/master/src/INSTALL
-# sudo apt install -y \
-#     git make clang libtool-bin libxt-dev libgtk-3-dev libpython3-dev valgrind
+# Neovim
+# https://github.com/neovim/neovim/blob/master/BUILD.md#build-prerequisites
+sudo apt install -y --no-install-recommends \
+    ninja-build gettext cmake unzip curl
 
-# # Neovim
-# # https://github.com/neovim/neovim/blob/master/BUILD.md#build-prerequisites
-# sudo apt install -y \
-#     ninja-build gettext cmake unzip curl
-
-# # tmux
-# # https://github.com/tmux/tmux/wiki/Installing#building-dependencies
-# sudo apt install -y \
-#     libevent-dev ncurses-dev build-essential bison pkg-config \
-#     libutf8proc-dev libutempter0
-
-# Tools
-# sudo apt install --install-suggests -y vim neovim zsh tmux
+# tmux
+# https://github.com/tmux/tmux/wiki/Installing#building-dependencies
+sudo apt install -y --no-install-recommends \
+    libevent-dev ncurses-dev build-essential bison pkg-config \
+    libutf8proc-dev libutempter0
