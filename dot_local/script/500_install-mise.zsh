7373aa2159cd3bb79be3720273f1f52dc2a56270
k5-mot <34744243+k5-mot@users.noreply.github.com>
2025-07-02 23:49:35 +0900

    A

--- dot_local/script/500_install-mise.zsh
+++ dot_local/script/500_install-mise.zsh
@@ -1,12 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # mise
 curl https://mise.run | sh
+~/.local/bin/mise --version
 export PATH="$HOME/.local/bin:$PATH"
 
 mise --version
 eval "$(~/.local/bin/mise activate zsh)"
-mise reshim
-mise doctor
+# mise reshim
+# mise doctor
 mise install -y
 mise reshim
